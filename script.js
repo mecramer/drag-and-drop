0685d73c5fafc788ee3614ff51fc6cf953e61d22
Cramer <cramerm@nih.gov>
2020-03-03 19:59:08 -0500

    Adding sort order to the list, chaining .map, .sort, forEach

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // grab DOM elements
 const draggable_list = document.querySelector('#draggable-list');
 const check = document.querySelector('#check');
@@ -23,13 +25,22 @@ let dragStartIndex;
 createList();
 
 // Insert List items into DOM
-// we use the spread operator which makes a copy of the array
+// we use the spread operator which makes a copy of the array richestPeople
+// .map() is used turn into an object with a value (the person) and a random number assigned to sort
+// .sort() is then used to put the array in numerical order of the sort value for the objects
+// we then map it back into an array of strings with another map, returning the value (person)
 // we then create a list item for the current item being evaluated
 // then we want to set a data attribute for the li and give it the current index number in the forEach
 // we then build the html inside the list item including the person and index numbeer
 // finally we push the item we just created to the listItems array and then we append it to the page element
 function createList() {
-  [...richestPeople].forEach((person, index) => {
+  [...richestPeople]
+  .map(a => ({ value: a, sort: Math.random() }))
+  .sort((a, b) => a.sort - b.sort)
+  .map(a => a.value)
+  .forEach((person, index) => {
+    // console.log(person);
+
     const listItem = document.createElement('li');
 
     listItem.setAttribute('data-index', index);
